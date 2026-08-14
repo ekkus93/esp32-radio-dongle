fe2407f5e4e009894e03f8c0461fbb5143f053a2
Phillip Chin <ekkus93@gmail.com>
2026-08-14 16:37:03 -0700

    V1-401: define S3 bridge entry point

---
+++ firmware/esp32s3_usb_bridge/main/s3_bridge.h
@@ -0,0 +1,16 @@
+#ifndef S3_BRIDGE_H
+#define S3_BRIDGE_H
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t s3_bridge_start(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
