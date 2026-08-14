5099e93b18b9c11a7446e1ee6562b643fb016f58
Phillip Chin <ekkus93@gmail.com>
2026-08-14 16:24:03 -0700

    V1-301: define WROOM bridge entry point

---
+++ firmware/esp32_wroom_bt_controller/main/wroom_bridge.h
@@ -0,0 +1,16 @@
+#ifndef WROOM_BRIDGE_H
+#define WROOM_BRIDGE_H
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t wroom_bridge_start(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
