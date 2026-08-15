69021c184e8f227dcdbec4db4e44b2eed5a38391
Phillip Chin <ekkus93@gmail.com>
2026-08-14 17:36:15 -0700

    V1-104: add shared UART smoke-test interface

---
+++ firmware/components/radio_uart_smoke/include/radio_uart_smoke.h
@@ -0,0 +1,16 @@
+#ifndef RADIO_UART_SMOKE_H
+#define RADIO_UART_SMOKE_H
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t radio_uart_smoke_run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
