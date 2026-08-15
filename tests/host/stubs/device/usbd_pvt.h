9ff7e67b48cbf319c3333947039c02d83a6dce56
Phillip Chin <ekkus93@gmail.com>
2026-08-15 00:26:48 -0700

    style: format USB class host stubs

--- tests/host/stubs/device/usbd_pvt.h
+++ tests/host/stubs/device/usbd_pvt.h
@@ -12,10 +12,8 @@ typedef struct {
     bool (*deinit)(void);
     void (*reset)(uint8_t rhport);
     uint16_t (*open)(uint8_t rhport, const tusb_desc_interface_t *interface, uint16_t max_len);
-    bool (*control_xfer_cb)(uint8_t rhport, uint8_t stage,
-                            const tusb_control_request_t *request);
-    bool (*xfer_cb)(uint8_t rhport, uint8_t ep_addr, xfer_result_t result,
-                    uint32_t xferred_bytes);
+    bool (*control_xfer_cb)(uint8_t rhport, uint8_t stage, const tusb_control_request_t *request);
+    bool (*xfer_cb)(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, uint32_t xferred_bytes);
     void *xfer_isr;
     void *sof;
 } usbd_class_driver_t;
